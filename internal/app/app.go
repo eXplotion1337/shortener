49dfe542693597ccf38a3dae675318eba15c7082
eXplotion1337 <118596149+eXplotion1337@users.noreply.github.com>
2023-08-22 10:25:34 +0700

    test iter1

---
+++ internal/app/app.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	
+	"shortener/internal/app/handlers/service/repository"
+	"shortener/internal/app/handlers"
+	"shortener/internal/config"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Run(config *config.Config, storage repository.Storage) error {
+	r := chi.NewRouter()
+
+	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		handlers.PostAddURL(w, r, config, storage)
+	})
+
+	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.GetByID(w, r, config, storage)
+	})
+
+	log.Printf("Сервер запущен на %s", config.ServerAddr)
+	log.Printf("Base URL  %s", config.BaseURL)
+	log.Printf("Файл для сохранения данных расположен %s", config.StoragePath)
+	log.Printf("База данных  %s", config.DataBaseDSN)
+	log.Printf("Хранение данных реализовано через  %s", config.TypeStorage)
+
+	err := http.ListenAndServe(config.ServerAddr, r)
+	if err != nil {
+		log.Fatal("Ошибка старта сервера", err)
+		return err
+	}
+
+	return nil
+}
+
+func InitStorage(conf *config.Config) repository.Storage {
+	var storage repository.Storage
+	if conf.TypeStorage == "in-memory" {
+		storage = &repository.JSON{}
+		fmt.Println("in-memory")
+	} else if conf.TypeStorage == "file" {
+		fmt.Println("file")
+	} else {
+		fmt.Println("db")
+	}
+	return storage
+}
